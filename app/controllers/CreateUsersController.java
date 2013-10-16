7c6c0113a611e286feb97b5334ddc311f86bc882
Zeeshan Ali Ansari <ansari.zeeshanali@gmail.com>
2013-10-16 19:24:17 +0530

    Added default delete user and edit user api/method

--- app/controllers/CreateUsersController.java
+++ app/controllers/CreateUsersController.java
@@ -4,8 +4,9 @@ import models.userManagementDAO.Users;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
-import teamOneApi.TeamOneGUIAPI;
-import views.html.*;
+import teamOne.common.TeamOneGUIAPI;
+import views.html.userManagement.createUser;
+import views.html.userManagement.displayAllUsers;
 
 public class CreateUsersController extends Controller{
 	private static TeamOneGUIAPI api = new TeamOneGUIAPI();
@@ -26,13 +27,29 @@ public class CreateUsersController extends Controller{
 		 }
 		 
 		created.save();
-		//TODO:need to route it to diff page that show that the user is created
-		return ok("hi");
+		filledForm = null;
+		
+		return redirect(routes.CreateUsersController.displayAllUsers());
 	}
 	
 	public static Result displayAllUsers(){
-	
 		return ok(displayAllUsers.render(api.displayAllUsers()));
+	}
+	
+	public static Result deleteUser(long id){
+		api.deleteUserById(id);
+		return redirect(routes.CreateUsersController.displayAllUsers());
+	}
+	
+	public static Result editUser(Long id){
+		Form<Users> filledForm = signUpForm.bindFromRequest();
+		if(filledForm.hasErrors()){
+			 return badRequest(createUser.render(filledForm));
+		 }
+		//TODO need to remove this constant that has been attached to id
+		 id = 2l;
+		filledForm.get().update(id);
+		return redirect(routes.CreateUsersController.displayAllUsers());
 	}
 
 }
